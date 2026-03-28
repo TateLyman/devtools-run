94014599f685fc0191742dad3afdab09f0dddabf
Tate Lyman <lymantate2@gmail.com>
2026-03-27 20:00:10 -0500

    Fix image-to-pdf Blob type

--- app/image-to-pdf/page.tsx
+++ app/image-to-pdf/page.tsx
@@ -215,7 +215,7 @@ export default function ImageToPdfPage() {
       }
 
       const pdfBytes = await pdfDoc.save();
-      const blob = new Blob([pdfBytes], { type: "application/pdf" });
+      const blob = new Blob([new Uint8Array(pdfBytes) as BlobPart], { type: "application/pdf" });
       const url = URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.href = url;
